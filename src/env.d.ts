f548adb6c0efa530e84797fbe0b28478f0ac56c9
David Cuspoca <david-cuspoca@hotmail.com>
2026-06-02 20:20:16 -0600

    chore: agregar declaracion de tipos para NG_APP_SITE_URL

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly NG_APP_ENV?: string;
   readonly NG_APP_SUPABASE_URL?: string;
   readonly NG_APP_SUPABASE_ANON_KEY?: string;
+  readonly NG_APP_SITE_URL?: string;
 }
 
 interface ImportMeta {
